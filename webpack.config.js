e0e373bbdb8178dc6e10bfc151af1c02af230247
dveenkamp <daniel.veenkamp@gmail.com>
2018-09-15 17:06:05 -0400

    Set library target to umd

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,8 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'index.js',
-        library: 'reduxAtoms'
+        library: 'reduxAtoms',
+        libraryTarget: 'umd'
     },
     mode: 'none',
     module: {
